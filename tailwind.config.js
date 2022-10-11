d22227ce4c2453981b570c3681e9afae595516c6
Bryan Muller <brysn.ms@gmail.com>
2022-10-11 13:39:53 -0300

    Implementação do Header e Footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily:{
+      "snas": ["Poppins", "Helvetica", "Arial", "sans-serif"],
+    },
+    colors:{
+      "white": "#FFFFFF",
+      "black": "#000000",
+      "yellow": "#F8B10E",
+      "blue":"#21325F",
+      "gray":{"light":"#CCCCCC",
+    "dark":"#333333"}
+    },
     extend: {},
   },
   plugins: [],
